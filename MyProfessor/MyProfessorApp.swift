4787dba80ea79ed0bc85291781b7f5a31e4f84de
Al3j4ndrolm <lopezalejandro5b@gmail.com>
2025-01-23 14:40:27 -0800

    Initial Commit

---
+++ MyProfessor/MyProfessorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProfessorApp.swift
+//  MyProfessor
+//
+//  Created by Alejandro Lopez on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct MyProfessorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
