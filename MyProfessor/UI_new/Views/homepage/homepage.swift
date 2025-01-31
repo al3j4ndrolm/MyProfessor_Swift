59270b0e48ede58408b2297a51e7cf646b4a5c72
leonar21w <leonardsd.698@gmail.com>
2025-01-30 23:46:37 -0800

    Resolved merge conflicts in homepage.swift and homepage_header.swift

---
+++ MyProfessor/UI_new/Views/homepage/homepage.swift
@@ -0,0 +1,23 @@
+//
+//  homepage.swift
+//  MyProfessor
+//
+//  Created by Leonard Wibowo on 1/30/25.
+//
+
+import SwiftUI
+
+struct homepage: View {
+    var body: some View {
+        ZStack {
+            backgroundColor()
+            VStack {
+                homepage_header()
+            }
+        }
+    }
+}
+
+#Preview {
+    homepage()
+}
