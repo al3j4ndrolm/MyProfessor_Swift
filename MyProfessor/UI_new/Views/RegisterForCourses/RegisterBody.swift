be1207e9b439f0b2974e625cc570ee557510ff80
leonar21w <leonardsd.698@gmail.com>
2025-02-26 19:28:05 -0800

    Registration view UI updated

---
+++ MyProfessor/UI_new/Views/RegisterForCourses/RegisterBody.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterBody.swift
+//  MyProfessor
+//
+//  Created by Leonard Wibowo on 2/26/25.
+//
+
+import SwiftUI
+
+struct RegisterBody: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterBody()
+}
